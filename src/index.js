1d73a4d60267d6434698e749c99c5f86e8966ca7
Johhny <jhasse20@hotmail.com>
2021-12-10 00:32:29 -0300

    Implementações em middlwares

--- src/index.js
+++ src/index.js
@@ -8,21 +8,69 @@ app.use(express.json());
 app.use(cors());
 
 const users = [];
+const maxToDoPerFreeUser = 10;
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(u => u.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && (user.todos.length === maxToDoPerFreeUser)) {
+    return response.status(403).json({ error: "User reached the max number of ToDo for a free account" })
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params;
+
+  const isIdValid = validate(id);
+  if (!isIdValid) {
+    return response.status(400).json({ error: "The identification of ToDo is not valid" })
+  }
+
+  const user = users.find(u => u.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User must be logged in" })
+  }
+
+  const todo = user.todos.find(t => t.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "This ToDo do not belong to the user or do not exists" })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(u => u.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
